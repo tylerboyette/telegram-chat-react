e1cd530720ebd0ca426cfaec4e8077ea0a36f466
adriantelles <awesomewebdev01@gmail.com>
2018-01-16 20:47:24 +0300

    configure mongodb connection

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 + ~~Create Telegram Bot~~
 + ~~Install and configure node-telegram-api for listen some events on chats~~
-+ Install and configure MongoDB && MongoDB driver for node.js
++ ~~Install and configure MongoDB && MongoDB driver for node.js~~
 + Insert new users to MongoDB collection from listener 'message'
 + Install and configure express or something else
 + Add route to access to DB from front(CORS?)
--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-process.env["NTBA_FIX_319"] = 1;
-const TelegramBot = require('node-telegram-bot-api');
+// process.env["NTBA_FIX_319"] = 1;
+// const TelegramBot = require('node-telegram-bot-api');
 const config = require('./config');
 const MongoClient = require('mongodb').MongoClient;
-const bot = new TelegramBot(config.token, {polling: true});
-
+// const bot = new TelegramBot(config.token, {polling: true});
 console.log('START...');
+const uri = `mongodb://nzvtrk:${config.mongoPass}@cluster0-shard-00-00-obz3f.mongodb.net:27017,cluster0-shard-00-01-obz3f.mongodb.net:27017,cluster0-shard-00-02-obz3f.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
 
-var uri = `mongodb+srv://nzvtrk:${config.mongoPass}@cluster0-obz3f.mongodb.net/test`;
 MongoClient.connect(uri, function(err, client) {
   if(err){
-          return console.log(err);
-      }
-   const collection = client.db("test").collection("devices");
-   // perform actions on the collection object
-   client.close();
+    return console.log(err);
+  }
+  console.log(`Successful Connection to mongo!`);
+
+  const collection = client.db("test").collection("dd");
+  collection.stats().then(res => console.log(res))
+
+  client.close();
 });
 
-bot.on('message', (msg) => {
-  let firstName = msg.from.first_name || '';
-  let lastName = msg.from.last_name || '';
-  let fullName = `${firstName} ${lastName}`
-  let id = msg.from.id || '';
-  let username = `@${msg.from.username}` || '';
-  console.log(`${fullName} ${lastName} ${id} ${username} : ${msg.text}`);
- });
+// bot.on('message', (msg) => {
+//   let firstName = msg.from.first_name || '';
+//   let lastName = msg.from.last_name || '';
+//   let fullName = `${firstName} ${lastName}`
+//   let id = msg.from.id || '';
+//   let username = `@${msg.from.username}` || '';
+//   console.log(`${fullName} ${lastName} ${id} ${username} : ${msg.text}`);
+//  });
