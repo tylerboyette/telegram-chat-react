a15da5631003714c7a12782d07369b5b4f408c7e
adriantelles <awesomewebdev01@gmail.com>
2018-01-17 22:24:32 +0300

    add mongodb insert to eventEmitter

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 + ~~Create Telegram Bot~~
 + ~~Install and configure node-telegram-api for listen some events on chats~~
 + ~~Install and configure MongoDB && MongoDB driver for node.js~~
-+ Insert new users to MongoDB collection from listener 'message'
++ ~~Insert new users to MongoDB collection from listener 'message'~~
 + Install and configure express or something else
 + Add route to access to DB from front(CORS?)
 + Set up webhook(?)
--- app.js
+++ app.js
@@ -1,28 +1,55 @@
-// process.env["NTBA_FIX_319"] = 1;
-// const TelegramBot = require('node-telegram-bot-api');
+process.env["NTBA_FIX_319"] = 1;
+const TelegramBot = require('node-telegram-bot-api');
 const config = require('./config');
 const MongoClient = require('mongodb').MongoClient;
-// const bot = new TelegramBot(config.token, {polling: true});
+const bot = new TelegramBot(config.token, {polling: true});
 console.log('START...');
 const uri = `mongodb://nzvtrk:${config.mongoPass}@cluster0-shard-00-00-obz3f.mongodb.net:27017,cluster0-shard-00-01-obz3f.mongodb.net:27017,cluster0-shard-00-02-obz3f.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`;
 
-MongoClient.connect(uri, function(err, client) {
-  if(err){
-    return console.log(err);
-  }
-  console.log(`Successful Connection to mongo!`);
+let getCollection = (collection) => {
+  collection.find().toArray( (err,res)=>{
+    if (err) {
+      console.log(err);
+    }
+    console.log(res);
+  })
+}
 
-  const collection = client.db("test").collection("dd");
-  collection.stats().then(res => console.log(res))
+// db.collection("dd").remove({});
 
-  client.close();
+// MongoClient.connect(uri, function(err, cl ient) {
+//   if(err){
+//     return console.log(err);
+//   }
+//   console.log(`Successful Connection to mongo!`);
+//   const collection = client.db('test').collection("dd");
+//   getCollection(collection);
+//   client.close();
+// });
+
+
+
+
+bot.on('message', (msg) => {
+  let firstName = msg.from.first_name || '';
+  let lastName = msg.from.last_name || '';
+  let fullName = `${firstName} ${lastName}`
+  let id = msg.from.id || '';
+  let userName = `@${msg.from.username}` || '';
+
+  MongoClient.connect(uri, function(err, client) {
+    if(err){
+      return console.log(err);
+    }
+    console.log(`Successful Connection to mongo!`);
+    const collection = client.db('test').collection("dd");
+    collection.update(
+      {id:id},
+      {id:id,username:userName,fullname:fullName},
+      {upsert:true}
+    )
+    client.close();
+  });
+
+  console.log(`${fullName} ${lastName} ${id} ${userName} : ${msg.text}`);
 });
-
-// bot.on('message', (msg) => {
-//   let firstName = msg.from.first_name || '';
-//   let lastName = msg.from.last_name || '';
-//   let fullName = `${firstName} ${lastName}`
-//   let id = msg.from.id || '';
-//   let username = `@${msg.from.username}` || '';
-//   console.log(`${fullName} ${lastName} ${id} ${username} : ${msg.text}`);
-//  });
